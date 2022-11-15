50de7532161d6ede260589759cb181985b60aaf2
James Patrick Ryan A. Pasaforte <jamespatrick.pasaforte@g.msuiit.edu.ph>
2022-11-15 16:35:00 +0800

    Set out the scheme and the files that belong to it

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='microblog',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
